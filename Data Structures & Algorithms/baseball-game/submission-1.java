261d2392e64579411e3637cda128a7ea0dcac390
Safwan Shaikh <walterwhte112@gmail.com>
2026-07-30 23:40:37 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/baseball-game/submission-1.java
@@ -0,0 +1,28 @@
+ class Solution{
+public int calPoints(String[] operations) {
+    Stack<Integer> stack = new Stack<>();
+    
+    for (String op : operations) {
+        switch (op) {
+            case "+":
+                int top = stack.pop();
+                int newTop = top + stack.peek();
+                stack.push(top);
+                stack.push(newTop);
+                break;
+            case "D":
+                stack.push(stack.peek() * 2);
+                break;
+            case "C":
+                stack.pop();
+                break;
+            default:
+                stack.push(Integer.parseInt(op));
+        }
+    }
+    
+    int sum = 0;
+    for (int score : stack) sum += score;
+    return sum;
+}
+}
